94c3402feedbee5479a38fb41deb01ebde6a9922
“Edward <eminaya49@gmail.com>
2023-03-29 10:26:33 -0600

    created some example data for database in seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  # Signup
   root "sessions#new"
   get "/signup" => "users#new"
   post "/users" => "users#create"
 
+  #Login
   get "/login" => "sessions#new"
   post "/sessions" => "sessions#create"
   get "/logout" => "sessions#destroy"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+todo = Todo.create(
+  user_id: 1,
+  title: "Laundry",
+  description: "Do the laundry",
+  deadline: "03-31-2023",
+  completed: "No",
+)
+todo = Todo.create(
+  user_id: 1,
+  title: "Read",
+  description: "Finish chapter 2",
+  deadline: "04-01-2023",
+  completed: "No",
+)
+todo = Todo.create(
+  user_id: 2,
+  title: "Apply to Jobs",
+  description: "Clean up resume and send to company",
+  deadline: "04-05-2023",
+  completed: "No",
+)
+todo = Todo.create(
+  user_id: 2,
+  title: "Schedule Doctor's appt",
+  description: "Annual Physical",
+  deadline: "04-10-2023",
+  completed: "No",
+)
